a14b2d9295a272e3d9ed38a6084e737624fdd675
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-05-15 20:47:37 +0300

    mido: Update fingerprint and description

--- mdroid.mk
+++ mdroid.mk
@@ -25,5 +25,5 @@ BOARD_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/V9.2.1.0.NCFMIEK:user/release-keys" \
-    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.2.1.0.NCFMIEK release-keys"
+    BUILD_FINGERPRINT="xiaomi/mido/mido:7.0/NRD90M/V9.5.8.0.NCFMIFA:user/release-keys" \
+    PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.5.8.0.NCFMIFA release-keys"
