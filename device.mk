a6796eee23cc38157447c17dd9966b87542d7405
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-06-05 14:58:55 +0300

    mido: Build camera hidl service

--- device.mk
+++ device.mk
@@ -124,14 +124,12 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service \
+    camera.device@3.2-impl \
     camera.msm8953 \
     libmm-qcamera \
-    Snap
-
-PRODUCT_PACKAGES += \
-    camera.device@1.0-impl \
-    camera.device@3.2-impl \
-    android.hardware.camera.provider@2.4-impl \
+    Snap \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
