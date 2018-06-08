a90f98bd3d29f666efee5d1792774f57c46d4d4e
Serhij Kyryljan <kyryljan.serhij@gmail.com>
2018-06-08 22:32:03 +0300

    mido: Remove android.hardware.wifi@1.0-supplicant

--- device.mk
+++ device.mk
@@ -366,7 +366,6 @@ PRODUCT_COPY_FILES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    android.hardware.wifi@1.0-supplicant \
     libcld80211 \
     libqsap_sdk \
     libQWiFiSoftApCfg \
